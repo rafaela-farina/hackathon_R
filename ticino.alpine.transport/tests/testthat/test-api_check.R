cbf048600e1769ef4171fd9370ecd1995b5b8aea
RETRJ <retrj986@gmail.com>
2026-06-19 12:44:44 +0200

    test added

--- ticino.alpine.transport/tests/testthat/test-api_check.R
+++ ticino.alpine.transport/tests/testthat/test-api_check.R
@@ -1,5 +1,11 @@
-test_that("Check api sanity", {
-  df_resp <- get_routes(8505300, 8505000, "06/19/2026", "11:17", 5)
+test_that("Check get_routes()", {
+  df_resp <- get_routes(8505300, 8505000, "06/20/2026", "17:17", 5)
 
   expect_equal(nrow(df_resp), 5)
+})
+
+test_that("Check get_routes_all()", {
+  df_resp <- get_routes_all(8505300, "06/20/2026", "17:17", 1)
+
+  expect_equal(nrow(df_resp), 14)
 })
